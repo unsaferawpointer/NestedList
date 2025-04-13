3eccb40e762cd421e364c417e9a58ac45542bc48
Anton Cherkasov <unsaferawpointer@gmail.com>
2025-04-13 13:07:58 +0300

    [localization](iOS): Add Russian language support to detailed view

---
+++ iOS/iOS/Units/ItemDetails/DetailsLocalization.swift
@@ -0,0 +1,19 @@
+//
+//  DetailsLocalization.swift
+//  iOS
+//
+//  Created by Anton Cherkasov on 13.04.2025.
+//
+
+import Foundation
+
+struct DetailsLocalization {
+	let saveButtonTitle = String(localized: "save-item-title", table: "DetailsLocalizable")
+	let cancelButtonTitle = String(localized: "cancel-item-title", table: "DetailsLocalizable")
+	let propertiesSectionTitle = String(localized: "properties-section-title", table: "DetailsLocalizable")
+	let textfieldPlaceholder = String(localized: "textfield-placeholder", table: "DetailsLocalizable")
+	let notePlaceholder = String(localized: "note-placeholder", table: "DetailsLocalizable")
+	let markToggleTitle = String(localized: "mark-toggle-title", table: "DetailsLocalizable")
+	let sectionToggleTitle = String(localized: "section-toggle-title", table: "DetailsLocalizable")
+	let warningText = String(localized: "warning-text", table: "DetailsLocalizable")
+}
