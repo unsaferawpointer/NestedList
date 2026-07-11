ec336d9ce0ac8af95435fa07bf9187a1d13a7e5f
Anton Cherkasov <unsaferawpointer@gmail.com>
2026-07-11 23:34:45 +0300

    [refactoring]: Move ContentAnalyticsService to CorePresentati

--- CorePresentation/Sources/CorePresentation/Units/Content/ContentAnalyticsService.swift
+++ CorePresentation/Sources/CorePresentation/Units/Content/ContentAnalyticsService.swift
@@ -1,29 +1,30 @@
 //
 //  ContentAnalyticsService.swift
-//  iOS
+//  CorePresentation
 //
 //  Created by Anton Cherkasov on 11.07.2026.
 //
 
 import Analytics
-import CorePresentation
 
-protocol ContentAnalyticsServiceProtocol {
+public protocol ContentAnalyticsServiceProtocol {
 	func track(_ event: ContentAnalyticsEvent) async
 	func flush() async
 }
 
-final class ContentAnalyticsService {
+public final class ContentAnalyticsService {
 
 	// MARK: - DI
 
 	private let analytics: any AnalyticsServiceProtocol
 
-	init(analytics: any AnalyticsServiceProtocol) {
+	// MARK: - Initialization
+
+	public init(analytics: any AnalyticsServiceProtocol) {
 		self.analytics = analytics
 	}
 
-	convenience init(identityProvider: AnalyticsIdentityProvider = AnalyticsIdentityProvider()) {
+	public convenience init(identityProvider: AnalyticsIdentityProvider = AnalyticsIdentityProvider()) {
 		self.init(
 			analytics: AnalyticsService(
 				engine: AmplitudeService(),
@@ -37,11 +38,11 @@ final class ContentAnalyticsService {
 // MARK: - ContentAnalyticsServiceProtocol
 extension ContentAnalyticsService: ContentAnalyticsServiceProtocol {
 
-	func track(_ event: ContentAnalyticsEvent) async {
+	public func track(_ event: ContentAnalyticsEvent) async {
 		await analytics.track(event)
 	}
 
-	func flush() async {
+	public func flush() async {
 		await analytics.flush()
 	}
 }
